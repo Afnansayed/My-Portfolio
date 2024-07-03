201b2b0d713652de2c87b53ee68f9be613d2910e
Afnansayed <afnansayed1973@gmail.com>
2024-07-03 21:13:38 +0600

    Skils section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Aboutme from "../Aboutme/Aboutme";
 import Banner from "../Banner/Banner";
+import Skills from "../Skills/Skills";
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Aboutme></Aboutme>
+            <Skills></Skills>
         </div>
     );
 };
---
+++ src/Pages/Home/Skills/Skills.jsx
@@ -0,0 +1,54 @@
+import logo1 from '../../../assets/icons/icons8-css-96.png'
+import logo2 from '../../../assets/icons/icons8-express-js-96.png'
+import logo3 from '../../../assets/icons/physics.png'
+import logo4 from '../../../assets/icons/icons8-javascript-96.png'
+import logo5 from '../../../assets/icons/icons8-mongo-db-96.png'
+import logo6 from '../../../assets/icons/icons8-nodejs-96.png'
+import logo7 from '../../../assets/icons/icons8-html-96.png'
+import logo8 from '../../../assets/icons/icons8-tailwind-css-96.png'
+const Skills = () => {
+    return (
+        <div className="bg-[#0a192f]">
+            <div className='text-center text-2xl md:text-4xl py-5 '>
+                <h2 className='text-[#ccd6f6] font-semibold'>Technology that I know</h2>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-3 p-8 gap-5 md:gap-0'>
+                <div className='w-64 flex flex-col mx-auto bg-[#ccd6f6] shadow-xl rounded-xl p-3 border-b-8 border-[#6dafe0] outline-none hover:scale-110'>
+                    <img src={logo3} className='w-24 mx-auto' alt="" />
+                    <p className='text-[#0a192f] text-center text-2xl font-semibold'>React</p>
+                </div>
+                <div className='w-64 flex flex-col mx-auto bg-[#ccd6f6] shadow-xl rounded-xl p-3 border-b-8 border-[#ffd600] outline-none hover:scale-110'>
+                    <img src={logo4} className='w-24 mx-auto' alt="" />
+                    <p className='text-[#0a192f] text-center text-2xl font-semibold'>Javascript</p>
+                </div>
+                <div className='w-64 flex flex-col mx-auto bg-[#ccd6f6] shadow-xl rounded-xl p-3 border-b-8 border-[#689f38] outline-none hover:scale-110'>
+                    <img src={logo5} className='w-24 mx-auto' alt="" />
+                    <p className='text-[#0a192f] text-center text-2xl font-semibold'>MongoDB</p>
+                </div>
+                <div className='w-64 flex flex-col mx-auto bg-[#ccd6f6] shadow-xl rounded-xl p-3 border-b-8 border-[#689f38] outline-none hover:scale-110 mt-0 md:mt-6'>
+                    <img src={logo6} className='w-24 mx-auto' alt="" />
+                    <p className='text-[#0a192f] text-center text-2xl font-semibold'>Node.js</p>
+                </div>
+                <div className='w-64 flex flex-col mx-auto bg-[#ccd6f6] shadow-xl rounded-xl p-3 border-b-8 border-[#039be5] outline-none hover:scale-110 mt-0 md:mt-6'>
+                    <img src={logo2} className='w-24 mx-auto' alt="" />
+                    <p className='text-[#0a192f] text-center text-2xl font-semibold'>Express.js</p>
+                </div>
+                <div className='w-64 flex flex-col mx-auto bg-[#ccd6f6] shadow-xl rounded-xl p-3 border-b-8 border-[#00acc1] outline-none hover:scale-110 mt-0 md:mt-6'>
+                    <img src={logo8} className='w-24 mx-auto' alt="" />
+                    <p className='text-[#0a192f] text-center text-2xl font-semibold'>Tailwind</p>
+                </div>
+                <div className='w-64 flex flex-col mx-auto bg-[#ccd6f6] shadow-xl rounded-xl p-3 border-b-8 border-[#039be5] outline-none hover:scale-110 mt-0 md:mt-6'>
+                    <img src={logo1} className='w-24 mx-auto' alt="" />
+                    <p className='text-[#0a192f] text-center text-2xl font-semibold'>CSS</p>
+                </div>
+                <div className='w-64 flex flex-col mx-auto bg-[#ccd6f6] shadow-xl rounded-xl p-3 border-b-8 border-[#e65100] outline-none hover:scale-110 mt-0 md:mt-6'>
+                    <img src={logo7} className='w-24 mx-auto' alt="" />
+                    <p className='text-[#0a192f] text-center text-2xl font-semibold'>HTML</p>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
